589cd15eb915070eb19e7e3aefc31265cd0c0602
Thomas Gelf <thomas@gelf.net>
2016-06-21 12:22:04 +0200

    Ddo\Host/ServiceState: first sample implementations

---
+++ HostState.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Icinga\Module\Director\Ddo;
+
+class HostState extends DdoObject
+{
+    protected $table = 'ddo_host_state';
+
+    protected $keyName = 'checksum';
+
+    protected $defaultProperties = array(
+        // active ?
+        'checksum'              => null,
+        'host'                  => null,
+        'state'                 => null,
+        'state_type'            => null,
+        'hard_state'            => null,
+        'severity'              => null,
+        'acknowledged'          => null,
+        'in_downtime'           => null,
+        'last_update'           => null, // only on store if modified
+        'last_state_change'     => null,
+        'last_comment_checksum' => null,
+        'attempt'               => null,
+        'check_source_checksum' => null,
+    );
+
+    protected function getSortingState()
+    {
+        return self::$hostStateSortMap[$this->state];
+    }
+}
---
+++ ServiceState.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Icinga\Module\Director\Ddo;
+
+class ServiceState extends DdoObject
+{
+    protected $table = 'ddo_service_state';
+
+    protected $keyName = 'checksum';
+
+    protected $defaultProperties = array(
+        // active ?
+        'checksum'              => null,
+        'host_checksum'         => null,
+        'host'                  => null,
+        'service'               => null,
+        'state'                 => null,
+        'state_type'            => null,
+        'hard_state'            => null,
+        'attempt'               => null,
+        'severity'              => null,
+        'acknowledged'          => null,
+        'in_downtime'           => null,
+        'last_update'           => null, // only on store if modified
+        'last_state_change'     => null,
+        'last_comment_checksum' => null,
+        'check_source_checksum' => null,
+    );
+
+    protected function calculateSeverity()
+    {
+        $sev = parent::calculateSeverity();
+
+        // TODO: add host state to the mix
+
+        return $sev;
+    }
+
+    protected function getSortingState()
+    {
+        return self::$serviceStateSortMap[$this->state];
+    }
+}
