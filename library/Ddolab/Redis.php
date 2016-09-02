18aead2102dbd1d491bb4e3e9f4b6a53e83adebc
Thomas Gelf <thomas@gelf.net>
2016-09-02 15:06:13 +0000

    Redis: small factory

---
+++ library/Ddolab/Redis.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Icinga\Module\Ddolab;
+
+use Icinga\Application\Config;
+use Predis\Client as PredisClient;
+
+class Redis
+{
+    protected static $redis;
+
+    public static function instance($new = false)
+    {
+        if ($new || self::$redis === null) {
+            $config = Config::module('ddolab', 'config', true);
+
+            $options = array(
+                'host' => $config->get('redis', 'host', 'localhost'),
+                'port' => $config->get('redis', 'port', 6379),
+            );
+
+            if ($password = $config->get('redis', 'password', null)) {
+                $options['password'] = $password;
+            }
+
+            require_once dirname(__DIR__) . '/vendor/predis/autoload.php';
+            self::$redis = new PredisClient($options);
+        }
+        return self::$redis;
+    }
+}
