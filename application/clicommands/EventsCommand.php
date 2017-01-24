0ac8c990eb8d65e0718af47cee6c937d905c2b71
Thomas Gelf <thomas@gelf.net>
2017-01-24 09:19:22 +0100

    EventsCommand: set nice process titles

--- application/clicommands/EventsCommand.php
+++ application/clicommands/EventsCommand.php
@@ -2,7 +2,6 @@
 
 namespace Icinga\Module\Ddolab\Clicommands;
 
-use Icinga\Application\Logger;
 use Icinga\Module\Ddolab\Cli\Command;
 use Icinga\Module\Ddolab\IcingaEventHandler;
 use Icinga\Module\Ddolab\IcingaEventToRedisStreamer;
@@ -11,12 +10,14 @@ class EventsCommand extends Command
 {
     public function processAction()
     {
+        cli_set_process_title('ddolab/process');
         $handler = new IcingaEventHandler($this->ddo());
         $handler->processEvents();
     }
 
     public function streamAction()
     {
+        cli_set_process_title('ddolab/stream');
         $streamer = new IcingaEventToRedisStreamer($this->api());
         $streamer->stream();
     }
