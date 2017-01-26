782fd1d404c39612f46a3b2457e8b9d9794e4120
Eric Lippmann <eric.lippmann@netways.de>
2017-01-26 16:58:09 +0100

    Vagrant: Grant MySQL root access to MySQL containers from everywhere w/o password

--- vagrant/lxc/provision-mysql.sh
+++ vagrant/lxc/provision-mysql.sh
@@ -6,3 +6,13 @@ apt-get update
 apt-get dist-upgrade -y
 
 apt-get install -y mysql-server
+
+mysql -e "UPDATE user SET Host='%', plugin='mysql_native_password' WHERE User='root'; FLUSH PRIVILEGES" mysql
+
+cat <<EOF >/etc/mysql/mysql.conf.d/mysqld_bind.cnf
+[mysqld]
+bind-address = 0.0.0.0
+
+EOF
+
+systemctl restart mysql.service
