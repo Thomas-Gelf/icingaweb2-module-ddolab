bda5df325ce271ec2f29858c6d0b94cd4cc34407
Thomas Gelf <thomas@gelf.net>
2017-02-10 13:14:31 +0100

    StateSummaryBadges: introduce a new common base

--- library/Ddolab/Web/Component/HostStateSummaryBadges.php
+++ library/Ddolab/Web/Component/HostStateSummaryBadges.php
@@ -2,73 +2,14 @@
 
 namespace Icinga\Module\Ddolab\Web\Component;
 
-use Icinga\Module\Businessprocess\Html\BaseElement;
-use Icinga\Module\Businessprocess\Html\Element;
-use Icinga\Module\Businessprocess\Html\Link;
 use Icinga\Module\Ddolab\HostState;
 
-class HostStateSummary extends BaseElement
+class HostStateSummaryBadges extends StateSummaryBadges
 {
-    protected $contentSeparator = ' ';
+    protected $baseUrl = 'ddolab/hosts';
 
-    /** @var string */
-    protected $tag = 'div';
-
-    /** @inheritdoc */
-    protected $defaultAttributes = array(
-        'class' => 'statesummary',
-    );
-
-    public function __construct($states)
+    public function getStateName($state)
     {
-        $inUse = array();
-        foreach ($states as $state => $count) {
-            $stateName = HostState::hostSeverityStateName($state);
-            if (! array_key_exists($stateName, $inUse)) {
-                $inUse[$stateName] = array();
-            }
-
-            if (($state & HostState::FLAG_NONE) === HostState::FLAG_NONE) {
-                $inUse[$stateName]['unhandled'] = $count;
-            } else {
-                $inUse[$stateName]['handled'] = $count;
-            }
-        }
-
-        foreach ($inUse as $stateName => $counts) {
-            $ul = Element::create('ul', array('class' => $stateName));
-            if (array_key_exists('unhandled', $counts)) {
-                $ul->add(
-                    Element::create('li')->setContent(
-                        Link::create(
-                            $counts['unhandled'],
-                            'ddolab/hosts',
-                            array(
-                                'state'   => $stateName,
-                                'handled' => 'n'
-                            )
-                        )
-                    )
-                );
-            }
-
-            if (array_key_exists('handled', $counts)) {
-                $ul->add(
-                    Element::create('li', array('class' => 'handled'))
-                        ->setContent(
-                            Link::create(
-                                $counts['handled'],
-                                'ddolab/hosts',
-                                array(
-                                    'state'   => $stateName,
-                                    'handled' => 'y'
-                                )
-                            )
-                        )
-                );
-            }
-
-            $this->add($ul);
-        }
+        return HostState::hostSeverityStateName($state);
     }
 }
---
+++ library/Ddolab/Web/Component/StateSummaryBadges.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Icinga\Module\Ddolab\Web\Component;
+
+use Icinga\Exception\ProgrammingError;
+use Icinga\Module\Businessprocess\Html\BaseElement;
+use Icinga\Module\Businessprocess\Html\Element;
+use Icinga\Module\Businessprocess\Html\Link;
+use Icinga\Module\Ddolab\StateObject;
+
+abstract class StateSummaryBadges extends BaseElement
+{
+    protected $contentSeparator = ' ';
+
+    /** @var string */
+    protected $tag = 'div';
+
+    /** @inheritdoc */
+    protected $defaultAttributes = array(
+        'class' => 'statesummary',
+    );
+
+    /** @var string */
+    protected $baseUrl;
+
+    public function __construct($states)
+    {
+        $inUse = array();
+        foreach ($states->fetch() as $state => $count) {
+            $stateName = $this->getStateName($state);
+            if (! array_key_exists($stateName, $inUse)) {
+                $inUse[$stateName] = array();
+            }
+
+            if (($state & StateObject::FLAG_NONE) === StateObject::FLAG_NONE) {
+                $inUse[$stateName]['unhandled'] = $count;
+            } else {
+                $inUse[$stateName]['handled'] = $count;
+            }
+        }
+
+        foreach ($inUse as $stateName => $counts) {
+            $ul = Element::create('ul', array('class' => $stateName));
+            $this->addItem($ul, 'unhandled', $stateName, $counts, 'n');
+            $this->addItem($ul, 'handled', $stateName, $counts, 'y', array('class' => 'handled'));
+            $this->add($ul);
+        }
+    }
+
+    protected function addItem(Element $ul, $key, $stateName, $counts, $handled, $attrs = null)
+    {
+        if (array_key_exists($key, $counts)) {
+            $ul->add(
+                Element::create('li', $attrs)->setContent(
+                    $this->makeLink($counts[$key], $stateName, $handled)
+                )
+            );
+        }
+
+        return $this;
+    }
+
+    public function getBaseUrl()
+    {
+        if ($this->baseUrl === null) {
+            throw new ProgrammingError(
+                'StateSummaryBadges implementations need a baseUrl'
+            );
+        }
+
+        return $this->baseUrl;
+    }
+
+    protected function makeLink($count, $stateName, $handled)
+    {
+        return Link::create(
+            $count,
+            $this->baseUrl,
+            array(
+                'state'   => $stateName,
+                'handled' => $handled
+            )
+        );
+    }
+
+    abstract public function getStateName($state);
+}
